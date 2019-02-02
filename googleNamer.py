3dcf988770834ac27ed6be282276673521f65e0f
Vamshinath <vamshinath.m8@gmail.com>
2019-02-02 13:33:47 +0530

    scan dir fun added

--- googleNamer.py
+++ googleNamer.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
-import requests
+import requests,os,re,sys
 
+def scanFiles():
+
+    ofiles = {}
+    imgFiles=[]
+
+    for root, directories, filenames in os.walk('.'):
+        for filename in filenames:
+            if "9351" in filename:
+                continue
+            try:
+                    f=os.path.abspath(os.path.join(root,filename))
+                    fsz=os.stat(f).st_size
+                    if fsz < 10240:
+                        continue
+                    ofiles[f]=fsz
+            except Exception as e:
+                print(e)
+
+    files = sorted(ofiles.items(),key=lambda x:x[1],reverse=True)
+
+    for fl,_ in files:
+        flnm_lower=os.path.basename(fl.lower())
+        if ".jpg" in flnm_lower or ".png" in flnm_lower or ".jpeg" in flnm_lower:
+            imgFiles.append(fl)
+
+    return imgFiles
+        
 
 def getName(fl):
     searchUrl = 'http://www.google.hr/searchbyimage/upload'
@@ -22,6 +49,12 @@ def getName(fl):
 
 def main():
 
+    os.chdir(sys.argv[1])
+
+    files = scanFiles()
+
+    for img in files:
+        print(img)
     
 
 
